4824b2411a6eb6757859b89934f47250d7f68f39
Cristian Carreño <cristian@iMac.local>
2018-11-05 18:33:43 -0800

    simplificado

---
+++ moneyChange/Moneda.swift
@@ -0,0 +1,37 @@
+//
+//  Moneda.swift
+//  moneyChange
+//
+//  Created by Cristian Carreño on 05/11/2018.
+//  Copyright © 2018 Cristian Carreño. All rights reserved.
+//
+
+import Foundation
+
+
+
+public class Moneda{
+    private var nombre: String
+    private  var costo: Double
+    private var imgFondo: String
+    private var bandera: String
+    init(nombre: String, costo: Double, imgFondo: String, bandera: String) {
+        self.nombre = nombre
+        self.costo = costo
+        self.imgFondo = imgFondo
+        self.bandera = bandera
+    }
+    func getNombre()->String{
+        return nombre
+    }
+    func getCosto()->Double{
+        return costo
+    }
+    func getImg()->String{
+        return imgFondo
+    }
+    func getBandera()->String{
+        return bandera
+        
+    }
+}
--- moneyChange/ViewController.swift
+++ moneyChange/ViewController.swift
@@ -8,81 +8,119 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let money=["Dolar","Euro","Libra","Peso","Yen"]
 
+let estadosUnidos = Moneda(nombre: "Dolar", costo: 1.14, imgFondo: "mahatan", bandera:"banderaEEUU")
+
+let espanya = Moneda(nombre: "Euro", costo: 1, imgFondo: "espanya", bandera:"banderaEspanyola")
+
+let londres = Moneda(nombre: "Libra", costo: 0.87, imgFondo: "londres", bandera:"banderaLondinense")
+
+let chile = Moneda(nombre: "Peso", costo: 781.85, imgFondo: "chile", bandera:"banderaChilena")
+
+let japon = Moneda(nombre: "Yen", costo: 129.03, imgFondo: "japon", bandera:"banderaJapon")
+
+let arrayMonedas = [estadosUnidos,espanya,londres,chile,japon]
+
+var contador = 0
+
+var diccionarioMoneda = ["Dolar":1.14,
+                         "Euro":1,
+                         "Libra":0.87,
+                         "Peso":781.85,
+                         "Yen":129.03]
+
+
+
+
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var cantidad: UITextField!
+    @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var imagenFondo: UIImageView!
-    
     @IBOutlet weak var bandera: UIImageView!
-    
     @IBOutlet weak var nombreMoneda: UILabel!
-    
     @IBOutlet weak var precioMoneda: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      imagenFondo.image = UIImage(named: "mahatan")
-      nombreMoneda.text = "Dolar"
-      precioMoneda.text = "1.14$"
-     bandera.image = UIImage(named:"banderaEEUU")
+        imagenFondo.image = UIImage(named:estadosUnidos.getImg())
+        nombreMoneda.text = estadosUnidos.getNombre()
+        precioMoneda.text = String(estadosUnidos.getCosto())
+        bandera.image = UIImage(named:estadosUnidos.getBandera())
+   
+    }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return diccionarioMoneda.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return money[row];
+    }
+    
+  
+    
+    @IBAction func btnConvert(_ sender: Any) {
+     
+        var valorObtenido: Int = Int(cantidad.text!)!
+        
+        
+        
     }
 
+    
+  
+  
     @IBAction func btnNext(_ sender: Any) {
-        
-        if(imagenFondo.image == UIImage(named:"mahatan")){
-            imagenFondo.image = UIImage(named: "espanya")
-            nombreMoneda.text = "Euro"
-            precioMoneda.text = "166.386 pesetas"
-            bandera.image = UIImage(named:"banderaEspanyola")
-        }
-       else if(imagenFondo.image == UIImage(named:"espanya")){
-            imagenFondo.image = UIImage(named: "londres")
-            nombreMoneda.text = "Libra"
-            precioMoneda.text = "0,87£"
-            bandera.image = UIImage(named:"banderaLondinense")
-            
-            
-        }
-        else if(imagenFondo.image == UIImage(named:"londres")){
-            imagenFondo.image = UIImage(named: "chile")
-            nombreMoneda.text = "Peso"
-            precioMoneda.text = "781,85₱"
-            bandera.image = UIImage(named:"banderaChilena")
-        }
-        else if(imagenFondo.image == UIImage(named:"chile")){
-            imagenFondo.image = UIImage(named: "japon")
-            nombreMoneda.text = "Yen"
-            precioMoneda.text = "129,03¥"
-            bandera.image = UIImage(named:"banderaJapon")
+          contador += 1
+        if(contador < arrayMonedas.count){
+            imagenFondo.image = UIImage(named:arrayMonedas[contador].getImg())
+            nombreMoneda.text = arrayMonedas[contador].getNombre()
+            precioMoneda.text = String(arrayMonedas[contador].getCosto())
+            bandera.image = UIImage(named:arrayMonedas[contador].getBandera())
+
+        }else{
+            print("no hay más Monedas")
+            contador=arrayMonedas.count-1
         }
         
     }
     
     @IBAction func btnBefore(_ sender: Any) {
-        if(imagenFondo.image == UIImage(named:"espanya")){
-            imagenFondo.image = UIImage(named: "mahatan")
-            nombreMoneda.text = "Dolar"
-            precioMoneda.text = "1.14$"
-            bandera.image = UIImage(named:"banderaEEUU")
+        contador -= 1
+        if(contador <= -1){
+            print("no hay más Monedas lo siento")
+            contador=0
+        }else{
+           imagenFondo.image = UIImage(named:arrayMonedas[contador].getImg())
+           nombreMoneda.text = arrayMonedas[contador].getNombre()
+           precioMoneda.text = String(arrayMonedas[contador].getCosto())
+           bandera.image = UIImage(named:arrayMonedas[contador].getBandera())
         }
-       else if(imagenFondo.image == UIImage(named:"londres")){
-            imagenFondo.image = UIImage(named: "espanya")
-            nombreMoneda.text = "Euro"
-            precioMoneda.text = "166.386 pesetas"
-            bandera.image = UIImage(named:"banderaEspanyola")
-            
-        }
-        else if(imagenFondo.image == UIImage(named:"chile")){
-            imagenFondo.image = UIImage(named: "londres")
-            nombreMoneda.text = "Libra"
-            precioMoneda.text = "0,87£"
-            bandera.image = UIImage(named:"banderaLondinense")
-        }
-        else if(imagenFondo.image == UIImage(named:"japon")){
-            imagenFondo.image = UIImage(named: "chile")
-            nombreMoneda.text = "Peso"
-            precioMoneda.text = "781,85₱"
-            bandera.image = UIImage(named:"banderaChilena")
-        }
+       
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
